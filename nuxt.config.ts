df41192d7cfe8ff4796973810f2b7f32f31d4153
laoer536 <1547698569@qq.com>
2022-05-29 11:08:49 +0800

    pinia配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,28 @@
-import { defineNuxtConfig } from 'nuxt'
-
+import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    head:{
-        link: [
-            { rel: "icon", href: "/favicon.ico" },
-        ]
+  head: {
+    link: [{ rel: "icon", href: "/favicon.ico" }],
+  },
+  typescript: {
+    strict: true,
+    typeCheck: true,
+    tsConfig: {
+      types: ["@pinia/nuxt"],
     },
-    vite:{
-        server: {
-            proxy: {
-                '/api': {
-                    target: 'http://xxxxxxxx',
-                    rewrite: (path) => path.replace(/^\/api/, ''),
-                }
-            },
-        }
-
-},
-    buildModules: [
-        '@pinia/nuxt',
-    ],
-})
+  },
+  buildModules: ["@pinia/nuxt"],
+  router: {
+    middleware: ["dynamic-head"],
+  },
+  vite: {
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://xxxxxxxx",
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  },
+});

28a318f29b2c3ef731e1e722faa17ccfa2a1386a
laoer536 <1547698569@qq.com>
2023-02-05 15:17:33 +0800

    style: eslint & prettier

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-// prettier.config.js
-module.exports = {
-  semi: false,
-  singleQuote: true,
-  printWidth: 120,
-  htmlWhitespaceSensitivity: 'ignore',
-  plugins: [require('prettier-plugin-tailwindcss')],
-}
